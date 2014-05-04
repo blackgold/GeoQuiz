ad8b49e426f179fa7d732435de440e9266369fdd
adevendra23 <adevendra23@gmail.com>
2014-05-04 14:29:34 -0500

    toast

--- app/src/main/java/com/example/geoquiz/app/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/app/QuizActivity.java
@@ -4,14 +4,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mTrueButton = (Button)findViewById(R.id.buttonYes);
+        mTrueButton.setOnClickListener( new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Toast.makeText(QuizActivity.this,R.string.true_toast,Toast.LENGTH_SHORT).show();
+           }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.buttonNo);
+        mFalseButton.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this,R.string.false_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
